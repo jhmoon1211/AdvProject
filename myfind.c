47bf71e7606e444e9801dee8fe8552e006f6bf7d
Kim min sik <kms_14@naver.com>
2016-12-02 09:55:59 +0900

    no run perm

--- myfind.c
+++ myfind.c
@@ -9,9 +9,22 @@
 
 #include <stdio.h>
 #include <getopt.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
+#define BUF_SIZE 512
+int FindByPerm(char* path);
+char* newpath(char* path);
+char NewFile[30];
 int main(int argc, char *argv[]) {
-
+	char buf[BUF_SIZE];
+	int bufsize;
 	char *PathArr[100];
 	int PathNum=0;
 	char *CopyArgv[argc];
@@ -58,6 +71,12 @@ int main(int argc, char *argv[]) {
 					case 0:		// 옵션이 options[0]인 name일 경우
 						break;
 					case 1:		//perm인 경우
+						for(i=0;i<PathNum;i++){
+							FindByPerm(PathArr[i]);
+							getcwd(buf,bufsize);
+							chdir(buf);
+						}
+
 						break;
 					case 2:		//type인 경우
 						break;
@@ -81,4 +100,92 @@ int main(int argc, char *argv[]) {
 				}
 		}
 	}
+	return 0;
 }
+
+int FindByPerm(char* path){
+	DIR* DP;
+	struct stat FileStat;
+	char buf2[BUF_SIZE];
+	struct dirent *DirectStat;
+	FILE *fd;
+	if(access(path,R_OK)){
+		perror("access denied");
+		exit(1);
+	}
+	if(!(DP=opendir(path))){
+		path = (char *)newpath(path);
+		if(!(DP=opendir(path))){
+			perror("opendir error");
+			return -1;
+		}
+	}
+	chdir(path);
+
+	char * path_buf = (char*)malloc(BUF_SIZE);
+	
+	while((DirectStat = readdir(DP)) != NULL){
+		if(!(DirectStat->d_ino))
+			continue;
+		if(!strcmp(DirectStat->d_name, ".") || !strcmp(DirectStat->d_name,".."))
+			continue;
+		if(lstat(DirectStat->d_name, &FileStat)<0){
+			perror("lstat error");
+			return -1;
+		}
+
+		if(!strcmp(FileStat.st_mode,optarg))
+			continue;
+
+		printf("%s  %s\n",getcwd(path_buf,BUF_SIZE),DirectStat->d_name);
+	}
+	rewinddir(DP);
+	while((DirectStat=readdir(DP))!=NULL){
+		if(!(DirectStat->d_ino))
+			continue;
+		if(!strcmp(DirectStat->d_name, ".") || !strcmp(DirectStat->d_name,".."))
+			continue;
+		if(lstat(DirectStat->d_name, &FileStat)<0){
+			perror("lstat error");
+			return -1;
+		}
+
+		if((FileStat.st_mode & S_IFMT)==S_IFDIR){
+			FindByPerm(DirectStat->d_name);
+		}
+	}
+	closedir(DP);
+	chdir("..");
+	free(path_buf);
+	return 0;
+}
+
+char* newpath(char* path){
+	int len,i,j=0,k=0;
+	char path1[100];
+
+	len = strlen(path);
+	strcpy(path1,path);
+	for(i=len;i>0;i--){
+		if(path1[i]=='/'){
+			j=i;
+			break;
+		}
+	}
+	j++;
+	for (i=j;i<=len;i++){
+		NewFile[k++]=path1[i];
+		path1[i] = NULL;
+	}
+	NewFile[k] = NULL;
+	strcpy(path,path1);
+	return path;
+}
+		
+
+
+
+
+
+
+
