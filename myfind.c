c0ebc7f85cc7f1e0e7feccdf7e920036a35d2025
namjukim94 <namjukim94@gmail.com>
2016-12-08 00:58:15 +0900

    added

--- myfind.c
+++ myfind.c
@@ -9,11 +9,19 @@
 #include <limits.h>
 #include <unistd.h>
 #include <fnmatch.h>
-/* global variable declaration */
 #define BUF_SIZE 512
+
+/* global variable declaration */
+char * gfile_name; // a name of a file as a global variable
+int gcount;
+char * guser_name;
+char * lname; // login name
+
 /* global function declaration */
 void findFilesWithNameOfFile(char *, char *);
+void intoTheDirectory(char *);
 void findFilesWithNameOfUser(char *, char *);
+void printUserFiles(char *);
 void showHowToUseMYFIND();
 void group(char *, char *);
 void empty(char *);
@@ -25,27 +33,28 @@ int StringtoInt(char* str, int radix);
 int DeleteByName(char* path, char* arg);
 int MvByName(char* path, char* arg, char* arg2);
 char *tmparg1=NULL;
+
 /* main fucn that has argc, argv */
 int main(int argc , char *argv[]) {
-    static struct option long_options[] = {
-        {"name",   required_argument,        0, 'n'},
-        {"user",   required_argument,        0, 'u'},
-        {"perm",   required_argument,        0, 'p'},
-        {"size",   required_argument,        0, 's'},
-        {"delete", required_argument,        0, 'd'},
-        {"group",  required_argument,        0, 'g'},
-        {"type",   required_argument,        0, 't'},
-        {"mv",     required_argument,        0, 'm'},
-        {"exec",   required_argument,        0, 'x'},
-        {"help",         no_argument,        0, 'h'},
-        {"empty",        no_argument,        0, 'e'},
-        {0, 0, 0, 0}
-    };
-    int option_index = 0;
-    int opt = 0;
-    char * path = NULL;
-    char * nameOfFile = NULL;
-    char * nameOfUser = NULL;
+	static struct option long_options[] = {
+		{"name",   required_argument,        0, 'n'},
+		{"user",   required_argument,        0, 'u'},
+		{"perm",   required_argument,        0, 'p'},
+		{"size",   required_argument,        0, 's'},
+		{"delete", required_argument,        0, 'd'},
+		{"group",  required_argument,        0, 'g'},
+		{"type",   required_argument,        0, 't'},
+		{"mv",     required_argument,        0, 'm'},
+		{"exec",   required_argument,        0, 'x'},
+		{"help",         no_argument,        0, 'h'},
+		{"empty",        no_argument,        0, 'e'},
+		{0, 0, 0, 0}
+	};
+	int option_index = 0;
+	int opt = 0;
+	char * path = NULL;
+	char * nameOfFile = NULL;
+	char * nameOfUser = NULL;
 	char buf[BUF_SIZE];
 	int i;
 	int index =0;
@@ -53,223 +62,203 @@ int main(int argc , char *argv[]) {
 	char *size_arg = NULL;
 	char *mv_arg = NULL;
 
-    while (1) {
-        opt = getopt_long (argc, argv, "n:u:p:s:d:g:t:m:x:h:e", long_options, &option_index);
-        if (opt == -1) break;
-        switch (opt) {
-            case 'n': /* name */
-                path = argv[1];
-                nameOfFile = argv[3];
-                findFilesWithNameOfFile(path, nameOfFile);
-                break;
-            case 'u': /* user */
-                path = argv[1];
-                nameOfUser = argv[3];
-                findFilesWithNameOfUser(path, nameOfUser);
-                break;
-            case 'p': /* perm */
+	while (1) {
+		opt = getopt_long (argc, argv, "n:u:p:s:d:g:t:m:x:h:e", long_options, &option_index);
+		if (opt == -1) break;
+		switch (opt) {
+			case 'n': /* name */
+				if (strncmp("--name", argv[2], 6) == 0) {
+					path = argv[1];
+					nameOfFile = argv[3];
+					findFilesWithNameOfFile(path, nameOfFile);
+				}
+				else {
+					printf("Confirm option \n");
+					exit(1);
+				}
+
+				break;
+			case 'u': /* user */
+				path = argv[1];
+				nameOfUser = argv[3];
+				findFilesWithNameOfUser(path, nameOfUser);
+				break;
+			case 'p': /* perm */
 				path = argv[1];
 				perm_arg = optarg;
 				tmparg1=argv[3];
 				FindByPerm(path, perm_arg);
-                break;
-            case 's':
+				break;
+			case 's':
 				path = argv[1];
 				size_arg = optarg;
 				tmparg1=argv[3];
 				FindBySize(path, size_arg);
-                break;
-            case 'd':
+				break;
+			case 'd':
 				path = argv[1];
 				nameOfFile = argv[3];
 				DeleteByName(path, nameOfFile);
-                break;
-            case 'g':
-                printf("group \n");
-                group(argv[1], optarg);
-                break;
-            case 't':
-                printf("type \n");
+				break;
+			case 'g':
+				printf("group \n");
+				group(argv[1], optarg);
+				break;
+			case 't':
+				printf("type \n");
 				type(argv[1], optarg);
-                break;
-            case 'm':
+				break;
+			case 'm':
 				path=argv[1];
 				nameOfFile = argv[3];
 				mv_arg = argv[4];
 				MvByName(path, nameOfFile,mv_arg);
-                break;
-            case 'x':
-                printf("exec \n");
-                break;
-            case 'h':
-                printf("help \n");
-                showHowToUseMYFIND();
-                break;
-            case 'e':
-                printf("empty \n");
-                empty(argv[1]);
-                break;
-        }
-    }
-    return 0;
+				break;
+			case 'x':
+				printf("exec \n");
+				break;
+			case 'h':
+				showHowToUseMYFIND();
+				break;
+			case 'e':
+				printf("empty \n");
+				empty(argv[1]);
+				break;
+		}
+	}
+	return 0;
+}
+
+void intoTheDirectory(char * cwd) {
+	DIR * dp;
+	struct stat buf;
+	char * dir_new = NULL;
+	struct dirent *dent;
+	int kind;
+	char * n_cwd;
+	char wd[BUFSIZ];
+
+	if ((dp = opendir(cwd)) == NULL) exit(1);
+
+	while ((dent = readdir(dp))) {
+		stat(dent->d_name, &buf);
+		kind = buf.st_mode & S_IFMT;
+
+		if (dent->d_name[0] == '.') continue;
+
+		// regular file
+		if (strncasecmp(dent->d_name, gfile_name, gcount) == 0) {
+			if (kind == S_IFREG) {
+				printf("%s/%s\n", cwd, dent->d_name);
+			}
+		}
+		// directory
+		if (S_ISDIR(buf.st_mode)) {
+			chdir(dent->d_name);
+			n_cwd = getcwd(wd, BUFSIZ);
+			intoTheDirectory(n_cwd);
+		}
+	}
 }
 
 void findFilesWithNameOfFile(char * path, char * nameOfFile) {
-    char *cwd;
-    char wd[BUFSIZ];
-    char n_buf[BUFSIZ]; // This buf is for name of file
-    DIR * dp;
-    struct dirent *dent;
-    char * name[80]; // This is to find files users want
-    int i = 0;
-    int n = 0;
-    int len = 0;
-    int check = 0; // Whether or not there is any file a user wants to find
+	char *cwd1, *cwd;
+	char wd[BUFSIZ];
+	char n_buf[BUFSIZ]; // This buf is for name of file
+	int j = 0;
 
-    //  printf("PATH : %s \n", path); // path
-    //  printf("NAMEOFFILE : %s \n", nameOfFile); // name of file
+	cwd1 = getcwd(NULL, BUFSIZ); // current directory
+	chdir(path); // change directory
+	cwd = getcwd(wd, BUFSIZ); // current directory after it is changed
 
-    cwd = getcwd(NULL, BUFSIZ);
-    //  printf("Crrent D = %s \n", cwd);
-    chdir(path);
-    cwd = getcwd(wd, BUFSIZ);
-    //  printf("Changed Crrent D = %s \n", cwd);
+	sprintf(n_buf, "%s", nameOfFile); // saved name of file in n_buf
 
-    if ((dp = opendir(cwd)) == NULL) {
-        exit(1);
-    }
-    // To count all files in the directory
-    while ((dent = readdir(dp))) {
-        // dent->d_name
-        // char d_name[]; This is the declaration of the struct in dirent.h file
-        // printf("NameOfFile : %s \n", dent->d_name);
-        name[i] = dent->d_name;
-        i++;
-    }
+	for (j=0; j<1024; j++) {
+		if (n_buf[j] == '\0') break;
+	}
 
-    sprintf(n_buf, "%s", nameOfFile); // saved name of file in n_buf
-    // ???????????????????????????????????
-    printf("first char of the name of the file : %c \n", n_buf[0]);
-    printf("first char of the name of the file : %s \n", n_buf);
+	gfile_name = nameOfFile;
+	gcount = j;
 
+	intoTheDirectory(cwd);
+}
 
-    printf("-------------------------------------------------------------------\n");
-    printf("Here is all files in the directory. \n");
-    // When the name of the file is only *
-    n = i;
-    for (i=0; i<n; i++) {
-        printf("%s/", cwd);
-        printf("%s", *(name+i));
-        printf("\n");
-    }
-    printf("-------------------------------------------------------------------\n");
+void printUserFiles(char * cwd) {
+	DIR * dp;
+	struct stat buf;
+	char * dir_new = NULL;
+	struct dirent *dent;
+	int kind = 0;
+	char * n_cwd = NULL;
+	char wd[BUFSIZ];
+	uid_t euid;
 
-    // have to compare between n_buf and *name
-    // char n_buf[80]; Vs char * name[80];
-    // The reason why I use strcasecmp func is to ignore lowercase and uppercase
-    len = sizeof(n_buf);
-    //  if (strcasecmp(n_buf, *(name+0), len) == 0) {
-    for (i=0; i<n; i++) {
-        if (strcasecmp(n_buf, *(name+i)) == 0) {
-            // same
-            printf("%s/", cwd);
-            printf("%s", *(name+i));
-            printf("\n");
-            check = 1;
-        }
-        else {
-            // not same
-        }
-    }
+	euid = geteuid(); // To get effective uid
 
-    if (check == 0) {
-        printf("no any file you want to fild \n");
-    }
-    check = 0;
+	if ((dp = opendir(cwd)) == NULL) exit(1);
 
+	while ((dent = readdir(dp))) {
+		stat(dent->d_name, &buf);
+		kind = buf.st_mode & S_IFMT;
+
+		if (dent->d_name[0] == '.') continue;
+
+		if (strcmp(lname, guser_name) == 0) {
+			// regular file
+			if (kind == S_IFREG) {
+				// user's uid and file's uid
+				if ((int)euid == (int)buf.st_uid) {
+					printf("%s/%s\n", cwd, dent->d_name);
+				}
+			}
+		}
+
+		// directory
+		if  (S_ISDIR(buf.st_mode)) {
+			//  printf("I am a directory \n");
+			chdir(dent->d_name);
+			n_cwd = getcwd(wd, BUFSIZ);
+			printUserFiles(n_cwd);
+		}
+	}
 }
 
 void findFilesWithNameOfUser(char * path, char * nameOfUser) {
-    char *cwd;
-    char wd[BUFSIZ];
-    char n_buf[BUFSIZ]; // This buf is for name of file
-    DIR * dp;
-    struct dirent *dent;
-    char * name[80]; // This is to find files users want
-    int i = 0;
-    int n = 0;
-    int len = 0;
-    int check = 0; // Whether or not there is any file a user wants to find
-    uid_t euid;
-    char * cname;
-    struct stat f_buf;
-    char * lname; // login name
+	char *cwd;
+	char wd[BUFSIZ];
+	DIR * dp;
+	struct dirent *dent;
+	uid_t euid;
+	char * cname;
 
-    lname = getlogin(); // To get login name
+	lname = getlogin(); // To get login name
+	euid = geteuid(); // To get effective uid
+	cname = cuserid(NULL);
 
-    euid = geteuid(); // To get effective uid
-    cname = cuserid(NULL);
+	cwd = getcwd(NULL, BUFSIZ);
+	chdir(path);
+	cwd = getcwd(wd, BUFSIZ);
 
-    cwd = getcwd(NULL, BUFSIZ);
-    chdir(path);
-    cwd = getcwd(wd, BUFSIZ);
+	guser_name = nameOfUser;
 
-    if ((dp = opendir(cwd)) == NULL) {
-        exit(1);
-    }
-    // To count all files in the directory
-    while ((dent = readdir(dp))) {
-        name[i] = dent->d_name;
-        i++;
-    }
-    n = i;
-
-    sprintf(n_buf, "%s", nameOfUser); // saved name of file in n_buf
-
-    //  printf("UserID : %s \n", n_buf);
-    // strcmp between login name 'lname' and the name of the user 'n_buf'
-    if (strcmp(lname, n_buf) == 0) {
-        //  printf("It is same \n");
-        for (i=0; i<n; i++) {
-            stat(*(name+i), &f_buf);
-            // To find files which effective user is the same as
-            if ((int)euid == (int)f_buf.st_uid) {
-                //  printf("USER's file %d : %d \n",i+1 , (int)f_buf.st_uid);
-                // All files in the directory
-                printf("%s/", cwd);
-                printf("%s", *(name+i));
-                printf("\n");
-                check = 1;
-            }
-        }
-    }
-    else {
-        printf("Confirm user id \n");
-        exit(1);
-    }
-
-
-    if (check = 0) {
-        printf("There is no files you find \n");
-    }
+	printUserFiles(cwd);
 }
 
 void showHowToUseMYFIND() {
-    fprintf(stderr,"======================================================================================\n");
-    fprintf(stderr, "|--- myfind [PATH] [OPTION] [___] ---| \n");
-    fprintf(stderr, "\t |- name [íŒŒì¼ì´ë¦„] : â€˜íŒŒì¼ì´ë¦„â€™ê³¼ ì¼ì¹˜í•˜ëŠ” íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- user [ìœ ì €ì´ë¦„] : â€˜ìœ ì €ì´ë¦„â€™ê³¼ ì¼ì¹˜í•˜ëŠ” íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- help : findëª…ë ¹ì–´ì— ëŒ€í•œ ì„¤ëª… ì¶œë ¥ \n");
-    fprintf(stderr, "\t |- perm [íŒŒì¼ê¶Œí•œ] : â€˜íŒŒì¼ê¶Œí•œâ€™ê³¼ ì¼ì¹˜í•˜ëŠ” íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- size [íŒŒì¼ìš©ëŸ‰] : â€˜íŒŒì¼ìš©ëŸ‰â€™ì´ìƒì˜ íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- delete [íŒŒì¼ì´ë¦„] : â€˜íŒŒì¼ì´ë¦„â€™ì— ì§€ì •í•œ íŒŒì¼ì„ ê²€ìƒ‰í•˜ê³  ì‚­ì œ \n");
-    fprintf(stderr, "\t |- group [ê·¸ë£¹ì´ë¦„] : â€˜ê·¸ë£¹ì´ë¦„â€™ê³¼ ì¼ì¹˜í•˜ëŠ” íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- type [íŒŒì¼ì¢…ë¥˜] : â€˜íŒŒì¼ì¢…ë¥˜â€˜ë¥¼ ì§€ì •í•˜ì—¬ íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- empty : ë¹ˆ íŒŒì¼ ê²€ìƒ‰ \n");
-    fprintf(stderr, "\t |- mv [íŒŒì¼ì´ë¦„] [ë””ë ‰í† ë¦¬ì´ë¦„] : â€˜íŒŒì¼ì´ë¦„â€™ì˜ íŒŒì¼ì„ ì°¾ì•„ì„œ ê²½ë¡œë¥¼ ì¶œë ¥í•˜ê³  \n");
-    fprintf(stderr, "\t |- â€˜ë””ë ‰í† ë¦¬ì´ë¦„â€™ì— ì§€ì •í•œ ë””ë ‰í† ë¦¬ë¡œ ì´ë™ \n");
-    // fprintf(stderr, "\t |- exec [ëª…ë ¹] {} \; : ì°¾ì€ íŒŒì¼ë“¤ì— ëŒ€í•œ íŠ¹ì • ëª…ë ¹ì„ ìˆ˜í–‰í•  ë•Œ ì‚¬ìš© \n");
-    fprintf(stderr,"======================================================================================\n");
+	fprintf(stderr,"======================================================================================\n");
+	fprintf(stderr, "|--- myfind [PATH] [OPTION] [___] ---| \n");
+	fprintf(stderr, "\t |- name [ÆÄÀÏÀÌ¸§] : ¡®ÆÄÀÏÀÌ¸§¡¯°ú ÀÏÄ¡ÇÏ´Â ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- user [À¯ÀúÀÌ¸§] : ¡®À¯ÀúÀÌ¸§¡¯°ú ÀÏÄ¡ÇÏ´Â ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- help : find¸í·É¾î¿¡ ´ëÇÑ ¼³¸í Ãâ·Â \n");
+	fprintf(stderr, "\t |- perm [ÆÄÀÏ±ÇÇÑ] : ¡®ÆÄÀÏ±ÇÇÑ¡¯°ú ÀÏÄ¡ÇÏ´Â ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- size [ÆÄÀÏ¿ë·®] : ¡®ÆÄÀÏ¿ë·®¡¯ÀÌ»óÀÇ ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- delete [ÆÄÀÏÀÌ¸§] : ¡®ÆÄÀÏÀÌ¸§¡¯¿¡ ÁöÁ¤ÇÑ ÆÄÀÏÀ» °Ë»öÇÏ°í »èÁ¦ \n");
+	fprintf(stderr, "\t |- group [±×·ìÀÌ¸§] : ¡®±×·ìÀÌ¸§¡¯°ú ÀÏÄ¡ÇÏ´Â ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- type [ÆÄÀÏÁ¾·ù] : ¡®ÆÄÀÏÁ¾·ù¡®¸¦ ÁöÁ¤ÇÏ¿© ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- empty : ºó ÆÄÀÏ °Ë»ö \n");
+	fprintf(stderr, "\t |- mv [ÆÄÀÏÀÌ¸§] [µð·ºÅä¸®ÀÌ¸§] : ¡®ÆÄÀÏÀÌ¸§¡¯ÀÇ ÆÄÀÏÀ» Ã£¾Æ¼­ °æ·Î¸¦ Ãâ·ÂÇÏ°í \n");
+	fprintf(stderr, "\t |- ¡®µð·ºÅä¸®ÀÌ¸§¡¯¿¡ ÁöÁ¤ÇÑ µð·ºÅä¸®·Î ÀÌµ¿ \n");
+	fprintf(stderr,"======================================================================================\n");
 }
 
 void group(char* path, char *arg) {
